58f4d0920dd66b9dd322f10cb38c4de48dbde336
Cheng-MaoMao <84213317+Cheng-MaoMao@users.noreply.github.com>
2024-12-09 16:03:50 +0800

    Update main.py
    优化程序逻辑，会强制最高音量

--- Sennpai/main.py
+++ Sennpai/main.py
@@ -3,9 +3,24 @@ from PIL import Image, ImageTk
 import pygame
 import time
 import threading
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 # 初始化pygame
 pygame.mixer.init()
+
+# 获取系统音量控制接口
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+# 解除静音状态
+volume.SetMute(0, None)
+
+# 设置音量为最大
+volume.SetMasterVolumeLevelScalar(1.0, None)
 
 # 创建主窗口，但先隐藏它
 window = tk.Tk()
@@ -24,9 +39,14 @@ def create_main_window():
     label = tk.Label(window, text="请尽情抚摸先辈(单击/长按)")
     label.pack()
 
-    # 加载图像
-    normal_image = ImageTk.PhotoImage(Image.open("resource/image/normal.jpg"))
-    apply_image = ImageTk.PhotoImage(Image.open("resource/image/apply.jpg"))
+    try:
+        # 加载图像
+        normal_image = ImageTk.PhotoImage(Image.open("resource/image/normal.jpg"))
+        apply_image = ImageTk.PhotoImage(Image.open("resource/image/apply.jpg"))
+    except Exception as e:
+        tk.messagebox.showerror("错误", f"无法加载图片: {e}")
+        window.destroy()
+        return
 
     # 创建按钮
     button = tk.Button(window, image=normal_image)
@@ -77,10 +97,21 @@ def create_main_window():
 
     # 播放音频的函数
     def play_sound(sound_file, button):
-        pygame.mixer.music.load(sound_file)
-        pygame.mixer.music.play()
-        while pygame.mixer.music.get_busy():  # 等待音频播放完成
+        try:
+            pygame.mixer.music.load(sound_file)
+            pygame.mixer.music.play()
+        except Exception as e:
+            tk.messagebox.showerror("错误", f"无法加载音频文件: {e}")
+            window.destroy()
+            return
+
+        # 音频播放期间持续设置系统音量为最大
+        while pygame.mixer.music.get_busy():
+            # 解除静音状态
+            volume.SetMute(0, None)
+            volume.SetMasterVolumeLevelScalar(1.0, None)
             time.sleep(0.1)
+
         if not pygame.mixer.music.get_busy():  # 音频播放完成后恢复默认图像
             button.config(image=normal_image)
 
@@ -112,10 +143,10 @@ info_text.pack()
 
 # 插入信息
 info_text.insert(tk.END, "本程序为作者在下北泽偶遇雷普高人，经高人指点后开发。\n")
-info_text.insert(tk.END, "使用程序前请调高音量以获得最佳体验。", "highlight")
+info_text.insert(tk.END, "使用程序前请确保身边没人，以防社死！！", "highlight")
 
 # 使用tag_configure方法来改变"highlight"标签的颜色和样式
-info_text.tag_configure("highlight", foreground="red", font=("TkDefaultFont", 10, "bold"))
+info_text.tag_configure("highlight", foreground="red", font=("TkDefaultFont", 20, "bold"))
 
 # 创建一个新的函数来处理窗口关闭事件
 def close_info_and_open_main():
